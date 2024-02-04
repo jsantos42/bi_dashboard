96fe1dbe44a03f16e2fe73b351bb3ebc68cfe51f
João Almeida Santos <79864209+jsantos42@users.noreply.github.com>
2024-02-04 14:38:47 +0100

    feat: add ScatterChart

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,11 @@
 import LineChart from '@/app/components/LineChart';
+import ScatterChart from '@/app/components/ScatterChart';
 
 export default function Home() {
 	return (
 		<>
 			<LineChart />
+			<ScatterChart />
 		</>
 	);
 }
