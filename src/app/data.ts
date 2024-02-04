278df0ce3554563a8b0b7e6a283a993471054156
João Almeida Santos <79864209+jsantos42@users.noreply.github.com>
2024-02-04 18:32:26 +0100

    feat: add more datasets to each plot

--- src/app/data.ts
+++ src/app/data.ts
@@ -6,10 +6,19 @@ import { ChartData, ChartOptions } from 'chart.js/auto';
 export const lineData: ChartData = {
 	datasets: [
 		{
-			label: 'Line Dataset',
+			label: 'Sample 1',
 			data: Array.from({ length: 100 }, () => {
 				const x = Math.random() * 100;
-				const y = x * 2;
+				const y = x * 1.4;
+
+				return { ...{ x, y } };
+			}),
+		},
+		{
+			label: 'Sample 2',
+			data: Array.from({ length: 100 }, () => {
+				const x = Math.random() * 100;
+				const y = x / 2;
 
 				return { ...{ x, y } };
 			}),
@@ -36,10 +45,28 @@ export const lineOptions: ChartOptions = {
 export const scatterData: ChartData = {
 	datasets: [
 		{
-			label: 'Line Dataset',
+			label: 'Sample 1',
 			data: Array.from({ length: 100 }, () => {
-				const x = Math.random() * 100;
+				const x = Math.cos(Math.random()) * 150;
+				const y = Math.random() * 100 * 1.25;
+
+				return { ...{ x, y } };
+			}),
+		},
+		{
+			label: 'Sample 2',
+			data: Array.from({ length: 100 }, () => {
+				const x = Math.random() * 100 * 1.25;
 				const y = Math.random() * 100;
+
+				return { ...{ x, y } };
+			}),
+		},
+		{
+			label: 'Sample 3',
+			data: Array.from({ length: 100 }, () => {
+				const x = Math.sqrt(Math.random()) * 50;
+				const y = Math.sqrt(x) * Math.random() * 10;
 
 				return { ...{ x, y } };
 			}),
