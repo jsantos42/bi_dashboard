c616f8f7ec9d4d6f8d48775e3a08d0ef1464b9b4
João Almeida Santos <79864209+jsantos42@users.noreply.github.com>
2024-02-05 12:10:15 +0100

    feat: add company name and font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,13 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Image from 'next/image';
+import localFont from 'next/font/local';
 
 const inter = Inter({ subsets: ['latin'] });
+const degular = localFont({
+	src: './fonts/DegularVariable.ttf',
+	display: 'swap',
+});
 
 export const metadata: Metadata = {
 	title: 'BI Dashboard',
@@ -27,6 +32,11 @@ export default function RootLayout({
 						width={112}
 						height={33}
 					/>
+					<span
+						className={`hidden sm:block mt-2 place-self-center ${degular.className}`}
+					>
+						aimed analytics
+					</span>
 				</nav>
 				<main className="flex justify-center items-center p-5 mt-8 md:p-4">
 					{children}
